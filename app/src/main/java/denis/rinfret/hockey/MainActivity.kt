3820f11a1edbf1da0d4eb38afab00c5378dc012a
profdenis <denis@profdenis.com>
2024-10-21 17:01:47 -0400

    l'expansion de la carte de joueur remplacée par un pop-up

--- app/src/main/java/denis/rinfret/hockey/MainActivity.kt
+++ app/src/main/java/denis/rinfret/hockey/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -18,11 +20,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Card
@@ -43,12 +47,14 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
 import denis.rinfret.hockey.data.HockeyPlayer
 import denis.rinfret.hockey.data.filterPlayers
 import denis.rinfret.hockey.data.getSamplePlayers
@@ -153,13 +159,13 @@ private fun SearchTextFields(
 
 @Composable
 fun HockeyPlayerCard(player: HockeyPlayer, modifier: Modifier = Modifier) {
-    var isExpanded by remember { mutableStateOf(false) }
+    var showPopup by remember { mutableStateOf(false) }
 
     Card(
         modifier = modifier
             .fillMaxWidth()
             .padding(16.dp)
-            .clickable { isExpanded = !isExpanded },
+            .clickable { showPopup = true },
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
         Column(
@@ -169,14 +175,32 @@ fun HockeyPlayerCard(player: HockeyPlayer, modifier: Modifier = Modifier) {
         ) {
             PlayerBasicData(player)
 
-            if (isExpanded) {
-                Spacer(modifier = Modifier.height(16.dp))
-                // Informations supplémentaires du joueur
+            if (showPopup) {
+                Popup(
+                    onDismissRequest = { showPopup = false },
+                    alignment = Alignment.Center
+                ) {
 
-
-                PlayerDetails(player)
-                ImageCarousel(photoResources = player.photoResources)
-
+                    Column(
+                        modifier = Modifier
+                            .width(400.dp)
+                            .background(
+                                color = MaterialTheme.colorScheme.secondaryContainer,
+                                shape = RoundedCornerShape(8.dp)
+                            )
+                            .border(
+                                4.dp,
+                                MaterialTheme.colorScheme.onSecondaryContainer,
+                                RoundedCornerShape(5.dp)
+                            )
+                            .padding(16.dp)
+                    ) {
+                        PlayerBasicData(player)
+                        Spacer(modifier = Modifier.height(16.dp))
+                        PlayerDetails(player)
+                        ImageCarousel(photoResources = player.photoResources)
+                    }
+                }
             }
         }
     }
@@ -288,7 +312,11 @@ fun ImageCarousel(photoResources: List<Int>) {
             .height(200.dp)
             .padding(8.dp)
     ) {
-        Box(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.secondaryContainer)
+        ) {
             LazyRow(
                 modifier = Modifier.fillMaxSize(),
                 horizontalArrangement = Arrangement.spacedBy(8.dp)
